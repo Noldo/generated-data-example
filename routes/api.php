01c8dd03a47c138f04b23ec86f98ee4fcc70b3c7
Андрей Скрынько <dumadunala@Mac-mini-Andrej.local>
2021-10-29 16:49:05 +0700

    install sanctum and configure authentication for api

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,9 +15,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
-
 Route::get('users', [\App\Http\Controllers\UsersController::class, 'getUsers']);
-Route::get('posts', [\App\Http\Controllers\PostsController::class, 'getPosts']);
+Route::get('posts', [\App\Http\Controllers\PostsController::class, 'getPosts'])->middleware('auth:sanctum');
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/me', [AuthController::class, 'me'])->middleware('auth:sanctum');
